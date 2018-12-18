75ddecfccbeb7e336673eca4e6b3b077426f6d75
Chabs <naimchabane@outlook.com>
2018-12-18 16:28:15 +0100

    Initialisation fini

--- js/game.js
+++ js/game.js
@@ -1,2 +1,17 @@
-console.log("Hello");
+window.onload = function() {
+    // Créations des boutons en variable
+    let play = document.getElementById('go');
+
+    console.log("Initialisation");
+
+    // Association boutons -> fonctions respective
+    play.onclick = init;
+
+    //Fonctions
+    function init() {
+        let pseudo = document.getElementById('Pseudo').value;
+        let budget = document.getElementById('Budget').value;
+        window.location = 'jeu.html';
+    }
+};
 
